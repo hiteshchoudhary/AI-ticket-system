dafea2432f4cd677678a15eadf7eac12165b49dc
Umesh Jadhav <ujadhav25@gmail.com>
2025-06-07 23:33:03 +0530

    await was missing
    await is required when you are hashing password using bcrypt

--- ai-ticket-assistant/controllers/user.js
+++ ai-ticket-assistant/controllers/user.js
@@ -6,7 +6,7 @@ import { inngest } from "../inngest/client.js";
 export const signup = async (req, res) => {
   const { email, password, skills = [] } = req.body;
   try {
-    const hashed = brcypt.hash(password, 10);
+    const hashed = await brcypt.hash(password, 10);
     const user = await User.create({ email, password: hashed, skills });
 
     //Fire inngest event
